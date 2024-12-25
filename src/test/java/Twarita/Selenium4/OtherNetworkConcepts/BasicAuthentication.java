be1a196b3f31d2f086a41abf0011731993c041a3
Twarita-lab <twarita.choudhury93@gmail.com>
2024-12-25 09:07:02 +0530

    Added  another method to handle windows authentication

--- src/test/java/Twarita/Selenium4/OtherNetworkConcepts/BasicAuthentication.java
+++ src/test/java/Twarita/Selenium4/OtherNetworkConcepts/BasicAuthentication.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicAuthentication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		WebDriver driver  = new ChromeDriver();
 		Predicate<URI> uriPredicate = URI->URI.getHost().contains("httpbin.org");
@@ -18,8 +18,14 @@ public class BasicAuthentication {
 		((HasAuthentication)driver).register(uriPredicate, UsernameAndPassword.of("foo","bar"));
 		
 		driver.get("https://httpbin.org/basic-auth/foo/bar");
+		Thread.sleep(2000);
+		
 		driver.get("https://httpbin.org/post");
 		
+		//The same can be done in below way. but in that case it won't be automatic checking and everytime needs to be hardcoaded.
+		//The below was available in Earlier version of Selenium
+		driver.get("https://foo:bar@httpbin.org/basic-auth/foo/bar");
+		Thread.sleep(2000);
 		driver.quit();
 		
 
